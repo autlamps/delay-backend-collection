a1be91bf79f7d1a80cb8a9dca42c68c4db17e276
Hayden Woodhead <hayden.sw@icloud.com>
2017-09-22 16:43:53 +1200

    Added json tags to output struct to enable usage by api

--- output/delays.go
+++ output/delays.go
@@ -9,34 +9,34 @@ import (
 
 // OutTrip is the final output for an individual trip running abnormally
 type OutTrip struct {
-	TripID         string
-	RouteID        string
-	RouteLongName  string
-	RouteShortName string
-	NextStop       NextStop
-	VehicleID      string
-	Lat            float64
-	Lon            float64
+	TripID         string   `json:"trip_id"`
+	RouteID        string   `json:"route_id"`
+	RouteLongName  string   `json:"route_long_name"`
+	RouteShortName string   `json:"route_short_name"`
+	NextStop       NextStop `json:"next_stop"`
+	VehicleID      string   `json:"vehicle_id"`
+	Lat            float64  `json:"lat"`
+	Lon            float64  `json:"lon"`
 }
 
 // NextStop is the information for the next stop of an abnormally running service
 type NextStop struct {
-	ID               string
-	Name             string
-	Lat              float64
-	Lon              float64
-	ScheduledArrival time.Time
-	Eta              time.Time
-	Delay            int
+	ID               string    `json:"id"`
+	Name             string    `json:"name"`
+	Lat              float64   `json:"lat"`
+	Lon              float64   `json:"lon"`
+	ScheduledArrival time.Time `json:"scheduled_arrival"`
+	Eta              time.Time `json:"eta"`
+	Delay            int       `json:"delay"`
 }
 
 // Out is the final output of 1 run of the collection service, ready to be saved into redis
 type Out struct {
-	Count      int
-	Trips      []OutTrip
-	ExecName   string
-	Created    int64
-	ValidUntil int64
+	Count      int       `json:"count"`
+	Trips      []OutTrip `json:"trips"`
+	ExecName   string    `json:"exec_name"`
+	Created    int64     `json:"created"`
+	ValidUntil int64     `json:"valid_until"`
 }
 
 func (o *Out) ToJSON() ([]byte, error) {
